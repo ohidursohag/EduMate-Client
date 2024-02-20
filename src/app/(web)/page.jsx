4d08d35ff38a4edc14f3a13be69cf4e3ae26110c
assadujjaman112 <assadujjamanmurad120@gmail.com>
2024-02-20 17:57:13 +0600

    Created our free courses section

--- src/app/(web)/page.jsx
+++ src/app/(web)/page.jsx
@@ -1,10 +1,13 @@
 import Blog from "@/Components/WebLayoutComponents/Home/Blog/Blog";
+import FreeCourses from "@/Components/WebLayoutComponents/Home/FreeCourses/FreeCourses";
+import CourseCard from "@/Components/WebLayoutComponents/Shared/CourseCard/CourseCard";
 
 const Homepage = () => {
 
   return (
     <div>
       <Blog />
+      <FreeCourses></FreeCourses>
     </div>
   )
 };
