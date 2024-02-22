0360a8c32ee87d39dce918d333c589d74872758f
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2024-02-22 14:16:23 +0600

    add logo image and icon and add font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,18 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
-const inter = Inter({ subsets: ["latin"] });
+const jost = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "EduMate",
   description: "e-learning management Application",
   icons: {
-    icon: "./fav.ico",
+    icon: "./logoIcon.png",
   },
 };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={jost.className} suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
